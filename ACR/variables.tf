43ba3acaeae1c44a604d5f8028c2c6ad5d9c4cfe
Gaurvssris <gauravsrivastwa85@gmail.com>
2026-07-08 22:24:41 +0530

    NewlyUpdated

--- ACR/variables.tf
+++ ACR/variables.tf
@@ -1,1 +1,9 @@
-variable "acr" {}
+variable "acr" {
+      type = map(object({
+    name                = string
+    sku                 = string
+    admin_enabled       = bool
+    resource_group_name = string
+    location            = string
+  }))
+}
--- AKS/variables.tf
+++ AKS/variables.tf
@@ -1,1 +1,11 @@
-variable "aks" {}
+variable "aks" {
+      type = map(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    dns_prefix          = string
+    kubernetes_version  = string
+    vm_size             = string
+    node_count          = number
+  }))
+}
