7e08c1af53061137917e00638bed484929fd37e4
Gaurvssris <gauravsrivastwa85@gmail.com>
2026-07-09 11:17:18 +0530

    updated2

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_kubernetes_cluster" "DumAks" {
+resource "azurerm_kubernetes_cluster" "dumaks" {
 
   for_each = var.aks
 
@@ -12,7 +12,6 @@ resource "azurerm_kubernetes_cluster" "DumAks" {
     name           = each.value.node_pool_name
     vm_size        = each.value.vm_size
     node_count     = each.value.node_count
-
     vnet_subnet_id = each.value.vnet_subnet_id
   }
 
@@ -22,7 +21,6 @@ resource "azurerm_kubernetes_cluster" "DumAks" {
 
   network_profile {
     network_plugin    = "azure"
-    network_policy    = "azure"
     load_balancer_sku = "standard"
   }
 }
